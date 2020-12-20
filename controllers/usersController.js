dd0d80ffaee5ae9c226235075a055bab6cb9669d
bulat kt <bulat.kt@yandex.ru>
2020-12-20 14:55:05 +0300

    cookie setup fix

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -56,6 +56,7 @@ const login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
+          sameSite: true,
         })
         .send({ message: 'Authorization success' });
     })
