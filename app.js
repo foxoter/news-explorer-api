f1d45ab454bc3c93b0462464e705ebfefa13b308
bulat kt <bulat.kt@yandex.ru>
2021-01-19 20:23:22 +0300

    cors setup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { MONGO_URL, PORT, mongooseOptions } = require('./config');
@@ -13,6 +14,19 @@ const routes = require('./routes/index');
 
 const app = express();
 
+const corsOptions = {
+  origin: [
+    'http://localhost:8080',
+    'https://foxoter.github.io',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(corsOptions));
 app.use(limiter);
 app.use(helmet());
 app.use(bodyParser.json());
